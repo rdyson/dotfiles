03b8e3d3f53064d9d9f92a8b63ebb6eb67ea1f2a
Rob Dyson <rdyson@hotmail.com>
2024-06-08 01:30:12 +0200

    Tried bgcolor, decided against it

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,24 +2,27 @@
 -- lazy nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Set bgcolor https://stackoverflow.com/a/73370407
+-- vim.api.nvim_set_hl(0, "Normal", { guibg = Black })
+
 require("vim-options")
 require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,22 +1,22 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    local lazy_status = require("lazy.status")
-    require('lualine').setup({
-      options = {
-        theme = 'dracula'
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    }) 
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		local lazy_status = require("lazy.status")
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+					},
+					-- { "encoding" },
+					-- { "fileformat" },
+					{ "filetype" },
+				},
+			},
+		})
+	end,
 }
