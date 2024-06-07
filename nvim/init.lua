9b339885cfafee3b09d0ccb57fe6e0714b16d27c
Rob Dyson <rdyson@hotmail.com>
2024-06-08 00:36:10 +0200

    Completion, LSP, trouble

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,7 +14,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
-require("lazy").setup("plugins", {
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
