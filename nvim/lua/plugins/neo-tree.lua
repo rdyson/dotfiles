c8c4eb34e3f6eba6da83a31e7f1dca150fdb7c74
Rob Dyson <rdyson@hotmail.com>
2024-06-08 02:58:26 +0200

    Rounded neotree border

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,24 +1,24 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          never_show = {},
-        },
-      }
-    })
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			popup_border_style = "rounded",
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					never_show = {},
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal float toggle<CR>", {})
+	end,
 }
-
