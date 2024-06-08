565130ad1a089739d2b349f531c0e9b1d6d9e508
Rob Dyson <rdyson@hotmail.com>
2024-06-08 10:26:13 +0200

    README neovim

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -28,26 +28,26 @@ opt.clipboard:append("unnamedplus")
 local keymap = vim.keymap
 
 --- navigate panes using vim keys
-keymap.set('n', '<c-k>', ':wincmd k<CR>')
-keymap.set('n', '<c-j>', ':wincmd j<CR>')
-keymap.set('n', '<c-h>', ':wincmd h<CR>')
-keymap.set('n', '<c-l>', ':wincmd l<CR>')
+keymap.set("n", "<c-k>", ":wincmd k<CR>")
+keymap.set("n", "<c-j>", ":wincmd j<CR>")
+keymap.set("n", "<c-h>", ":wincmd h<CR>")
+keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 --- clear search
-keymap.set('n', '<leader>h', ':nohlsearch<CR>', { desc = 'Clear search' })
+keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search" })
 
 --- splits
-keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split vertically' })
-keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split horizontally' })
-keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
-keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current split' })
+keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 --- tabs
-keymap.set('n', '<leader>to', '<cmd>tabnew<CR>', { desc = 'New tab' })
-keymap.set('n', '<leader>tx', '<cmd>tabclose<CR>', { desc = 'Close tab' })
-keymap.set('n', '<leader>tn', '<cmd>tabn<CR>', { desc = 'Next tab' })
-keymap.set('n', '<leader>tp', '<cmd>tabp<CR>', { desc = 'Previous tab' })
-keymap.set('n', '<leader>tf', '<cmd>tabnew %<CR>', { desc = 'Open file in new tab' })
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open file in new tab" })
 
 --- source
-keymap.set('n', '<leader>so', '<cmd>source %<CR>')
+keymap.set("n", "<leader>so", "<cmd>source %<CR>")
