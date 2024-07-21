753e9ff0b5fbf77674c83a11bc2942e04bed1572
Rob Dyson <rdyson@hotmail.com>
2024-07-21 05:59:47 +0100

    Remove extensions

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -47,5 +47,8 @@ keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+--- select all
+keymap.set("n", "<leader>a", "ggVG", { desc = "Select all" })
+
 --- source
 keymap.set("n", "<leader>so", "<cmd>source %<CR>")
--- vscode/extensions/jeppeandersen.vscode-kafka-0.15.0/docs/README.md
+++
@@ -1,8 +0,0 @@
-# Tools for Apache Kafka® Documentation
-
-Welcome to the [Tools for Apache Kafka®](https://github.com/jlandersen/vscode-kafka) documentation.
-
-* [Kafka Explorer](Explorer.md#explorer)
-* [Kafka file](KafkaFile.md#kafkafile)
-* [Producing messages](Producing.md#producing-messages)
-* [Consuming messages](Consuming.md#consuming-messages)
